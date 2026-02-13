5069ad8dd08e2ba2015aac235966ed66eb200820
Noahpayne32 <np571@exeter.ac.uk>
2026-02-13 12:01:47 +0000

    Task 1

---
+++ CopyArrayApp.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+public class CopyArrayApp
+{
+    
+  public static void main(String[] args) 
+  {
+    int nVals = args.length;
+    int[] myVals = new int[nVals];
+    ArrayList<Integer> uniqueVals = new ArrayList<>(nVals);
+    for (int i = 0; i < nVals; i++) 
+    {
+        
+      int item = Integer.parseInt(args[i]);
+      myVals[i] = item;
+      if (!uniqueVals.contains(item))
+      {
+        uniqueVals.add(item);
+      }
+      System.out.println(item);
+    }
+    System.out.println("original values: " + Arrays.toString(myVals));
+    System.out.println("Unique values:" + uniqueVals);
+  }
+}
